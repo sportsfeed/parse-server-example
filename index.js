5aa526781a5b12fdcbe3ed81db1fe14744a5df0b
Mike Wadhera <mikewadhera@gmail.com>
2016-04-25 13:57:41 -0700

    Add push config to server start

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  push: {
+    android: {
+      senderId: process.env.GCM_SENDER_ID || '',
+      apiKey: process.env.GCM_API_KEY || ''
+    },
+    ios: {
+      pfx: process.env.APNS_P12_PATH || '/app/push.p12',
+      bundleId: process.env.APNS_BUNDLE_ID || '',
+      production: true
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
